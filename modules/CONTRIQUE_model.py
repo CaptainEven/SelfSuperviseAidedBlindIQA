7f7a4a65bbc0a99e3bb3931a0215663a93022ff1
Even <765305261@qq.com>
2022-03-22 17:53:19 +0800

    Add Darknet backbone.

--- modules/CONTRIQUE_model.py
+++ modules/CONTRIQUE_model.py
@@ -36,9 +36,50 @@ class DarknetModel(nn.Module):
 
     def forward(self, x_i, x_j):
         """
-        forward pass
+        inference
         """
-        pass
+        # global features
+        h_i = self.encoder(x_i)
+        h_j = self.encoder(x_j)
+
+        # local features
+        h_i_patch = self.avgpool_patch(h_i)
+        h_j_patch = self.avgpool_patch(h_j)
+
+        h_i_patch = h_i_patch.reshape(-1, self.n_features, \
+                                      self.patch_dim[0] * self.patch_dim[1])
+
+        h_j_patch = h_j_patch.reshape(-1, self.n_features, \
+                                      self.patch_dim[0] * self.patch_dim[1])
+
+        h_i_patch = torch.transpose(h_i_patch, 2, 1)
+        h_i_patch = h_i_patch.reshape(-1, self.n_features)
+
+        h_j_patch = torch.transpose(h_j_patch, 2, 1)
+        h_j_patch = h_j_patch.reshape(-1, self.n_features)
+
+        h_i = self.avgpool(h_i)
+        h_j = self.avgpool(h_j)
+
+        h_i = h_i.view(-1, self.n_features)
+        h_j = h_j.view(-1, self.n_features)
+
+        if self.normalize:  # L2 normalize?
+            h_i = nn.functional.normalize(h_i, dim=1)
+            h_j = nn.functional.normalize(h_j, dim=1)
+
+            h_i_patch = nn.functional.normalize(h_i_patch, dim=1)
+            h_j_patch = nn.functional.normalize(h_j_patch, dim=1)
+
+        # global projections
+        z_i = self.projector(h_i)
+        z_j = self.projector(h_j)
+
+        # local projections
+        z_i_patch = self.projector(h_i_patch)
+        z_j_patch = self.projector(h_j_patch)
+
+        return z_i, z_j, z_i_patch, z_j_patch, h_i, h_j, h_i_patch, h_j_patch
 
 
 class CONTRIQUE_model(nn.Module):
