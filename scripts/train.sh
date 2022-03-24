2b084262f80c3d8a931c1a6384fc6e7c78951072
Even <765305261@qq.com>
2022-03-24 14:39:10 +0800

    Add Darknet backbone.

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,5 @@
 python3 ./train.py --lr 0.2 \
-                   --reload True \
                    --epoch_num 4 \
-                   --load_optimizer True \
-                   --print_freq 1
+                   --print_freq 1 \
+                   --reload True \
+                   --load_optimizer True
